0cc1edd8f6fa6ed39b41ebd00012eee4b284f21a
Balraj Singh Bhullar <bhullar.bs@gmail.com>
2022-05-11 14:58:35 +0530

    WIP: modified code to fetch numbers from String

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,9 +1,22 @@
-add = (x, y) => {
-    if(x<0 || y<0){
-        return "negative not allowed"
-    } else {
-        return x+y;
+add = (x) => {
+
+    numArr = x.match(/-?\d+/g).map(Number);
+
+    sum = 0;
+    for(i of numArr){
+        console.log(i)
+        sum += i;
     }
+
+    return sum;
+    
+    // if(x<0 || y<0){
+    //     return "negative not allowed"
+    // } else {
+    //     return x+y;
+    // }
 }
+
+console.log(add("1,4,-5"));
 
 module.exports={add}
